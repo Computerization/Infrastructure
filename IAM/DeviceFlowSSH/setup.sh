8f820be98ad02a12d94a90d84a83efc9b0016e48
qwerzl <me@qwerzl.me>
2024-09-22 15:47:10 +0800

    (DeviceFlowSSH): use `.conf` file to override defaults

--- IAM/DeviceFlowSSH/setup.sh
+++ IAM/DeviceFlowSSH/setup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i 's/@include common-auth/#@include common-auth\nauth required deviceflow.so/' /etc/pam.d/sshd
-sed -i 's/@include common-password/#@include common-password/'  /etc/pam.d/sshd
-sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
+cp ./20-computerization.conf /etc/ssh/sshd_config.d/
 cp ./deviceflow.so /lib/security
